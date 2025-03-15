35f0ac25ba4a5f486c8d5b60dac4f5bbb1527fa0
IvanTurgenheb <dltkdwls60202@gmail.com>
2025-03-15 21:39:10 +0900

    페이징 처리

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,22 +2,30 @@ const con = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
 const getBooks = (req, res, next) => {
-  const { category_id, newBooks } = req.query;
+  const { category_id, newBooks, limit, currentPage } = req.query;
 
   let sql = "SELECT * FROM books";
+  let sqlValues = [];
 
   if (category_id && newBooks) {
     sql +=
-      " LEFT JOIN category ON books.category_id = category.id WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW();";
+      " LEFT JOIN category ON books.category_id = category.id WHERE category_id = ? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
+    sqlValues.push(category_id);
   } else if (newBooks) {
     sql +=
-      " WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW();";
+      " WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
   } else if (category_id) {
     sql +=
       " LEFT JOIN category ON books.category_id = category.id WHERE category_id = ?";
+    sqlValues.push(category_id);
   }
 
-  con.query(sql, [category_id], (err, results) => {
+  if (limit && currentPage) {
+    sql += " LIMIT ? OFFSET ?";
+    sqlValues.push(Number(limit), Number(currentPage - 1) * Number(limit));
+  }
+
+  con.query(sql, sqlValues, (err, results) => {
     if (err) return next(err);
 
     if (results.length === 0) {
